130db2e56b7fb75ee6e3ac61ca56ba57483855c7
Saâd Qriouet <72690197+Qrtsaad@users.noreply.github.com>
2022-01-26 21:26:36 +0100

    Update one.simu_time.R

--- R/one.simu_time.R
+++ R/one.simu_time.R
@@ -43,6 +43,27 @@ one.simu_time_ocOP <- function(K, n)
   return(sim)
 }
 
+#' one.simu_time_ocOPcpp
+#'
+#'
+#' @description one simulation function for onechange OPcpp
+#'
+#' @param K number of changepoint
+#' @param n size of each "segment" of the data
+#'
+#' @return the time used to realise one simulation
+#' @export
+#'
+#' @examples
+#' one.simu_time_ocOPcpp(2, 1000)
+one.simu_time_ocOPcpp <- function(K, n)
+{
+  data <- dataSeries(K, n = 100, probs = sample(1:9,K)/10)
+  sim <- as.numeric(system.time(onechangeOPcpp(data))[3])
+  
+  return(sim)
+}
+
 #' one.simu_time_ocPELT
 #'
 #'
@@ -60,6 +81,27 @@ one.simu_time_ocPELT <- function(K, n)
 {
   data <- dataSeries(K, n = 100, probs = sample(1:9,K)/10)
   sim <- as.numeric(system.time(onechangePELT(data))[3])
+  
+  return(sim)
+}
+
+#' one.simu_time_ocPELTcpp
+#'
+#'
+#' @description one simulation function for onechange PELTcpp
+#'
+#' @param K number of changepoint
+#' @param n size of each "segment" of the data
+#'
+#' @return the time used to realise one simulation
+#' @export
+#'
+#' @examples
+#' one.simu_time_ocPELTcpp(2, 1000)
+one.simu_time_ocPELTcpp <- function(K, n)
+{
+  data <- dataSeries(K, n = 100, probs = sample(1:9,K)/10)
+  sim <- as.numeric(system.time(onechangePELTcpp(data))[3])
   
   return(sim)
 }
@@ -85,6 +127,27 @@ one.simu_time_OP <- function(K, n)
   return(sim)
 }
 
+#' one.simu_time_OPcpp
+#'
+#'
+#' @description one simulation function for OPcpp
+#'
+#' @param K number of changepoint
+#' @param n size of each "segment" of the data
+#'
+#' @return the time used to realise one simulation
+#' @export
+#'
+#' @examples
+#' one.simu_time_OPcpp(2, 1000)
+one.simu_time_OPcpp <- function(K, n)
+{
+  data <- dataSeries(K, n = 100, probs = sample(1:9,K)/10)
+  sim <- as.numeric(system.time(myOPcpp(data))[3])
+  
+  return(sim)
+}
+
 #' one.simu_time_PELT
 #'
 #'
@@ -102,6 +165,27 @@ one.simu_time_PELT <- function(K, n)
 {
   data <- dataSeries(K, n = 100, probs = sample(1:9,K)/10)
   sim <- as.numeric(system.time(myPELT(data))[3])
+  
+  return(sim)
+}
+
+#' one.simu_time_PELTcpp
+#'
+#'
+#' @description one simulation function for PELTcpp
+#'
+#' @param K number of changepoint
+#' @param n size of each "segment" of the data
+#'
+#' @return the time used to realise one simulation
+#' @export
+#'
+#' @examples
+#' one.simu_time_PELTcpp(2, 1000)
+one.simu_time_PELTcpp <- function(K, n)
+{
+  data <- dataSeries(K, n = 100, probs = sample(1:9,K)/10)
+  sim <- as.numeric(system.time(myPELTcpp(data))[3])
   
   return(sim)
 }
@@ -127,6 +211,27 @@ one.simu_time_CUSUM <- function(K,n)
   return(sim)
 }
 
+#' one.simu_time_CUSUMcpp
+#'
+#'
+#' @description one simulation function for CUSUMcpp
+#'
+#' @param K number of changepoint
+#' @param n size of each "segment" of the data
+#'
+#' @return the time used to realise one simulation
+#' @export
+#'
+#' @examples
+#' one.simu_time_CUSUMcpp(2, 1000)
+one.simu_time_CUSUMcpp <- function(K,n)
+{
+  data <- dataSeries(K, n = 2000, probs = sample(1:9,K)/10)
+  sim <- as.numeric(system.time(cusumcpp(data))[3])
+  
+  return(sim)
+}
+
 #' one.simu_time_TRV
 #'
 #'
@@ -141,6 +246,27 @@ one.simu_time_CUSUM <- function(K,n)
 #' @examples
 #' one.simu_time_TRV(2, 1000)
 one.simu_time_TRV <- function(K,n)
+{
+  data <- dataSeries(K, n = 2000, probs = sample(1:9,K)/10)
+  sim <- as.numeric(system.time(print("En cours"))[3])
+  
+  return(sim)
+}
+
+#' one.simu_time_TRVcpp
+#'
+#'
+#' @description one simulation function for TRVcpp
+#'
+#' @param K number of changepoint
+#' @param n size of each "segment" of the data
+#'
+#' @return the time used to realise one simulation
+#' @export
+#'
+#' @examples
+#' one.simu_time_TRVcpp(2, 1000)
+one.simu_time_TRVcpp <- function(K,n)
 {
   data <- dataSeries(K, n = 2000, probs = sample(1:9,K)/10)
   sim <- as.numeric(system.time(print("En cours"))[3])
@@ -165,6 +291,27 @@ one.simu_time_EMV <- function(K,n)
 {
   data <- dataSeries(K, n = 2000, probs = sample(1:9,K)/10)
   sim <- as.numeric(system.time(tau_EMV(data, tresh=0.5))[3])
+  
+  return(sim)
+}
+
+#' one.simu_time_EMVcpp
+#'
+#'
+#' @description one simulation function for EMVcpp
+#'
+#' @param K number of changepoint
+#' @param n size of each "segment" of the data
+#'
+#' @return the time used to realise one simulation
+#' @export
+#'
+#' @examples
+#' one.simu_time_EMVcpp(2, 1000)
+one.simu_time_EMVcpp <- function(K,n)
+{
+  data <- dataSeries(K, n = 2000, probs = sample(1:9,K)/10)
+  sim <- as.numeric(system.time(tau_EMVcpp(data, tresh=0.5))[3])
   
   return(sim)
 }
