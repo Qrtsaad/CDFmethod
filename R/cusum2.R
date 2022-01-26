c2e82229bb48b8984dc4e4cf6065e1c6cfa72bcb
T. Nadir <94757742+nado5@users.noreply.github.com>
2022-01-26 02:17:47 +0100

    ajout des benchmark et version dataset

--- R/cusum2.R
+++ R/cusum2.R
@@ -2,35 +2,105 @@ source(file = "scorefct.R")
 library(tictoc)
 library(GuessCompx)
 
+DIM = function(x) if(is.null(dim(x))) length(x) else dim(x)[1]
+
+constuct_data = function(n,tau,p1,p2) {
+  nb_test = length(p1)
+  mat_X = matrix(data=NA,nrow = nb_test,ncol=n)
+  for(i in 1:nb_test) {
+    x1 = rbinom(n=tau[i],size=1,prob=p1[i])
+    x2 = rbinom(n=n-tau[i],size=1,prob=p2[i])
+    mat_X[i,] = c(x1, x2)
+  }
+  return(mat_X)
+}
+
+
 cusum = function(X,a=0.05) {
-  n = length(X)
+  TIMER = as.numeric(Sys.time()) ####################
+  
   l = 1/10
   h = 9/10  # borne à définir (hypothèse : tau se trouve dans [l*n,h*n])
   
-  S = rep(0,n)
-  sigma_e = rep(0,n)
+    n = length(X)
+    S = sigma_e = rep(0,n)
+    
+    for(t in 1:n) {
+      S[t] = sum(X[1:t]) - t/n * sum(X)
+      sigma_e[t] =  t/n * (1 - t/n)
+    }
+    
+    curveCSM = S/sqrt(n)
+    
+    Tt = curveCSM[-n] / sigma_e[-n]
+    
+    CSMmax = abs(curveCSM) / sd(X)
+    tau = which.max(CSMmax)
+    
+    T_stat = max(Tt[round(l*n):round(h*n)]^2)
+    p_value = sqrt((T_stat*exp(-T_stat)/(2*pi))) * ((1-1/T_stat) * log((1-l)*h/(l*(1-h))) + 4/T_stat)
+    
+    if(is.nan(p_value)) {
+      y_pred = NaN
+      print(c("############# Erreur NaN p_value #############"))
+    }
+    else if(p_value <= a && p_value > 0) y_pred = 1
+    else y_pred = 0
   
-  for(t in 1:n) {
-    S[t] = sum(X[1:t]) - t/n * sum(X)
-    sigma_e[t] =  t/n * (1 - t/n)
+  TIMER = as.numeric(Sys.time() - TIMER) #######################
+  
+  return(list('Tt'=Tt,'curveCSM'=curveCSM,'tau'=tau,'CSMmax'=CSMmax,'sigma_e'=sigma_e,
+              'T_stat'=T_stat,'p_value'=p_value,'y_pred'=y_pred,'time'= TIMER))
+}
+
+multi_cusum = function(mat_X,a=0.05) {
+  nb_test = DIM(mat_X)[1]
+  y_pred = tau = p_value = times = rep(NA,nb_test)
+
+  for (j in 1:nb_test) {
+    CSM = cusum(mat_X[j,],a)
+    
+    y_pred[j] = CSM$y_pred
+    p_value[j] = CSM$p_value
+    tau[j] = CSM$tau
+    times[j] = CSM$time
+  }
+  return(list("y_pred"=y_pred,"p_value"=p_value,"tau"=tau,"times"=times))
+}
+
+mini_bench = function(n_seq,tau=-42) {
+  n_bench = length(n_seq)
+  times = rep(NA,n_bench)
+  
+  for(i in 1:n_bench) {
+    
+    p1 = runif(1,min=0.1,max=0.9)
+    p2 = runif(1,min=0.1,max=0.9)
+    
+    if(rbinom(1,size=1,prob=0.5)) p2 = p1
+    
+    if(tau == -42) tau2 = sample(1:n_seq[i],1)
+    else tau2 = tau[i]
+    
+    x1 = rbinom(n=tau2,size=1,prob=p1)
+    x2 = rbinom(n=n_seq[i]-tau2,size=1,prob=p2)
+    
+    X = t(matrix(c(x1,x2)))
+    CSM = cusum(X)
+    
+    times[i] = CSM$time
   }
   
-  curveCSM = S/sqrt(n)
+  plot(n_seq,times,type="l",col='black')
+  # points(n_seq,rep(1,n_bench),type="l",col='red')
+  # points(n_seq,n_seq,type="l",col='blue')
+  # points(n_seq,log(n_seq),type="l",col='yellow')
+  # points(n_seq,n_seq*log(n_seq),type="l",col='brown')
+  # points(n_seq,n_seq^0.5,type="l",col="green")
+  # points(n_seq,n_seq^2,type="l",col="pink")
+  # points(n_seq,n_seq^3,type="l",col="orange")
   
-  Tt = curveCSM[-n] / sigma_e[-n]
-  
-  CSMmax = abs(curveCSM) / sd(X)
-  tau = which.max(CSMmax)
-  
-  T_stat = max(Tt[round(l*n):round(h*n)]^2)
-  p_value = sqrt((T_stat*exp(-T_stat)/(2*pi))) * ((1-1/T_stat) * log((1-l)*h/(l*(1-h))) + 4/T_stat)
-  
-  if(is.nan(p_value)) y_pred = NaN
-  else if(p_value <= a && p_value > 0) y_pred = 1
-  else y_pred = 0
-
-  return(list('Tt'=Tt,'curveCSM'=curveCSM,'tau'=tau,'CSMmax'=CSMmax,'sigma_e'=sigma_e,
-              'T_stat'=T_stat,'p_value'=p_value,'y_pred'=y_pred))
+  return(times)
 }
 
 p1 = runif(1) 
@@ -41,40 +111,42 @@ n2 = 500
 x1 = rbinom(n1,1,p1)
 x2 = rbinom(n2,1,p2)
 X = c(x1,x2)
+X = t(as.matrix(X))
 
 CSM = cusum(X)
 plot(CSM$CSMmax,type='l')
 abline(v=CSM$tau,col='red')
+abline(v=500,col='green')
 print(c(p1,p2,"diff",abs(p1-p2)))
 print(c("p_value = ", CSM$p_value))
 print(c("tau = ",CSM$tau))
 print(c("class = ",CSM$y_pred))
 
-nb_test = 1000
-Y_true = rbinom(nb_test,1,0.5)
+
+
+nb_test = 50
+p1 = runif(nb_test,min=0.1,max=0.9)
+p2 = runif(nb_test,min=0.1,max=0.9)
+id = rbinom(nb_test,size=1,prob=0.5)
+p2[id==0] = p1[id==0]
+tau = rep(1000,nb_test)
+Y_true = !(p1 == p2)
 Y_pred = rep(0,nb_test)
-n1 = 500
-n2 = 500
+n = 2000
 
-tic("sleeping")
+mat_X = constuct_data(n,tau,p1,p2)
 
-for(i in 1:nb_test) {
-  p1 = runif(1) 
-  p2 = runif(1)
-  if(Y_true[i] == 0) p2=p1
-  x1 = rbinom(n1,1,p1)
-  x2 = rbinom(n2,1,p2)
-  X = c(x1,x2)
- 
-  CSM = cusum(X)
-  Y_pred[i] = CSM$y_pred
-  if(is.nan(Y_pred[i])) {
-    print(c("Erreur NaN ##########################################",p1,p2))
-    break
-  }
-}
-
-toc()
+TIMER = Sys.time()
+Y_pred = multi_cusum(mat_X)$y_pred
+TIMER = Sys.time() - TIMER
 
 print(compute_mf(Y_true,Y_pred,score="accuracy"))
 
+#CompEst(mat_X,multi_cusum)
+
+
+n_seq = seq(from=200,to=20000,by=200)
+mini_bench(n_seq)
+
+
+ 
