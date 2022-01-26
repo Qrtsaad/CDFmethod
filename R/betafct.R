4551e1fba2cabe535dd8c99effc5abc7fd153b5a
Saâd Qriouet <72690197+Qrtsaad@users.noreply.github.com>
2022-01-26 11:12:52 +0100

    Update betafct.R

--- R/betafct.R
+++ R/betafct.R
@@ -15,3 +15,22 @@ best_beta <- function(data)
   n <- length(data)
   return (2*var(data)*log(n))
 }
+
+
+#' BIC Beta
+#'
+#'
+#' @description BIC penalty
+#'
+#' @param data vector of data points
+#'
+#' @return BIC penalty value
+#' @export
+#'
+#' @examples
+#' BIC_beta(c(0,1,1,2,2,3))
+BIC_beta <- function(data)
+{
+  n <- length(data)
+  return(2*log(n))
+}
