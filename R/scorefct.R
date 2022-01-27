49252870fb08366c44cfff1274397d10672386df
Saâd Qriouet <72690197+Qrtsaad@users.noreply.github.com>
2022-01-27 03:50:11 +0100

    Update scorefct.R

--- R/scorefct.R
+++ R/scorefct.R
@@ -1,11 +1,5 @@
 prec_score <- function(mat)
 {
-  ###########
-  # TN # FN #
-  ###########
-  # FP # TP #
-  ###########
-  
   tp <- mat[2,2]
   fp <- mat[2,1]
   
@@ -15,29 +9,43 @@ prec_score <- function(mat)
 
 rec_score <- function(mat)
 {
-  ###########
-  # TN # FN #
-  ###########
-  # FP # TP #
-  ###########
-  
   tn <- mat[1,1]
   tp <- mat[2,2]
   
   return(tp/(tp+tn))
 }
 
+tpr_score <- function(mat){
+  tp <- mat[2,2]
+  fn <- mat[1,2] 
+  
+  return(tp/(tp+fn))
+}
+
+sensi_score <- function(mat)
+{
+  tp <- mat[2,2]
+  fn <- mat[1,2]
+  
+  return(tp/(tp+fn))
+}
+
+
+specif_score <- function(mat)
+{
+  tn <- mat[1,1]
+  fp <- mat[2,1]
+  
+  
+  return(tn/(tn+fp))
+}
+
 
 f1_score <- function(mat)
 {
-  ###########
-  # TN # FN #
-  ###########
-  # FP # TP #
-  ###########
-  
   precision <- prec_score(mat)
   recall <- rec_score(mat)
+  
   
   return(2*(precision*recall)/(precision+recall))
 }
@@ -45,32 +53,24 @@ f1_score <- function(mat)
 
 acc_score <- function(mat)
 {
-  ###########
-  # TN # FN #
-  ###########
-  # FP # TP #
-  ###########
-  
   tn <- mat[1,1]
   fn <- mat[1,2]
   fp <- mat[2,1]
   tp <- mat[2,2]
   
+  
   return((tn+tp)/(tn+fn+fp+tp))
 }
 
 
-
-
-
 tss_score <- function(mat)
 {
-  precision <- prec_score(mat)
-  recall <- rec_score(mat)
+  sensi <- sensi_score(mat)
+  speci <- specif_score(mat)
   
-  return(precision + recall - 1)
+  
+  return(sensi + speci - 1)
 }
-
 
 y_predict <- function(y, tau)
 {
@@ -96,39 +96,38 @@ nid_score <- function(y, y_pred)
 
 compute_mf <- function(v_ver,v_est, score = "f1")
 {
-  allowed.score <- c("f1", "accuracy", "precision", "recall", "tss", "nid")
-  if(!score %in% allowed.score){stop('type must be one of: ', paste(allowed.score, collapse=", "))}
-  
-  if (score == "f1") {score_f <- f1_score}
-  else if (score == "accuracy") {score_f <- acc_Score}
-  else if (score == "precision") {score_f <- prec_Score}
-  else if (score == "recall") {score_f <- rec_Score}
-  else if (score == "tss") {score_f <- tss_score}
-  else if (score == "nid") {score_f <- nid_score}
   
   # Initialisation de la matrice de confusion
-  #confusionmatrix <- matrix(data = 0, nrow=2,ncol=2)
-  #colnames(confusionmatrix) <- c("-", "+")
-  #rownames(confusionmatrix) <- c("-", "+")
+  confusionmatrix <- matrix(data = c(0,0,0,0), nrow=2,ncol=2)
   
+  colnames(confusionmatrix) <- c("-", "+")
+  rownames(confusionmatrix) <- c("-", "+")
   
-  #PREDITE\#REELLE
   ###########
   # TN # FN #
   ###########
   # FP # TP #
   ###########
   
-  confusionmatrix <- table(v_ver, v_est)
-  
-  #confusionmatrix[1,1] <- sum(v_ver == 0 & v_est == 0) #vrais négatifs
-  #confusionmatrix[1,2] <- sum(v_ver == 1 & v_est == 0) #faux négatifs
-  #confusionmatrix[2,1] <- sum(v_ver == 0 & v_est == 1) #faux positifs
-  #confusionmatrix[2,2] <- sum(v_ver == 1 & v_est == 1) #vrais positifs
+  confusionmatrix[1,1] <- sum((v_ver == FALSE) & (v_est == FALSE)) #vrais negatfs
+  confusionmatrix[1,2] <- sum((v_ver == TRUE) & (v_est == FALSE)) #faux négatifs
+  confusionmatrix[2,1] <- sum((v_ver == FALSE) & (v_est == TRUE)) #faux positifs
+  confusionmatrix[2,2] <- sum((v_ver == TRUE) & (v_est == TRUE)) #vrais positifs
   
   
-  if(score == "nid") {my_score <- nid_score(v_ver, v_est)} else {my_score <- score_f(confusionmatrix)}
-
+  
+  allowed.score <- c("f1", "accuracy", "tp_rate","specificite", "sensibilite", "tss", "precision", "recall")
+  if(!score %in% allowed.score){stop('type must be one of: ', paste(allowed.score, collapse=", "))}
+  
+  if (score == "f1") {my_score <- f1_score(confusionmatrix)}
+  else if (score == "accuracy") {my_score <- acc_score(confusionmatrix)}
+  else if (score == "tp_rate") {my_score <- tpr_score(confusionmatrix)}
+  else if (score == "specificite") {my_score <- specif_score(confusionmatrix)}
+  else if (score == "sensibilite") {my_score <- sensi_score(confusionmatrix)}
+  else if (score == "tss") {my_score <- tss_score(confusionmatrix)}
+  else if (score == "precision") {my_score <- prec_score(confusionmatrix)}
+  else if (score == "recall") {my_score <- rec_score(confusionmatrix)}
+  
   
   return(list(Confusion = confusionmatrix, score = my_score))
   
