2843f825e16f4d6659f13e65c49740df3c50cce0
Pablo_lgf <78906882+Pabzer@users.noreply.github.com>
2022-01-27 19:25:29 +0100

    Description fonctions

--- R/EMV.R
+++ R/EMV.R
@@ -1,4 +1,18 @@
-
+#' multi_EMV
+#'
+#'
+#' @description Applies tau_EMV to every row of a given matrix
+#'
+#' @param X with rows being vectors of Bernoilli data points
+#'
+#' @return A vector with length équal tu the number of rows of the given matrix. 
+#' Each element represents the prediction of fraud for the corresponding row with the tau_EMV method.
+#' @export
+#'
+#' @examples
+#' data <- constuct_data2(n = 2000, nsimu = 50)$data
+#' 
+#' multi_EMV(data)
 multi_EMV <- function(mat_X, fun = tau_EMV){
   
   #fun doit prendre pour valeur:
@@ -15,8 +29,54 @@ multi_EMV <- function(mat_X, fun = tau_EMV){
   return(list("y_pred"= y_pred))
 }
   
-
-#Choix du tau de rupture par maximum de vraissemblance
+#' multi_EMV_K
+#'
+#'
+#' @description Applies tau_EMV_K to every row of a given matrix
+#'
+#' @param X with rows being vectors of Bernoilli data points
+#'
+#' @return A vector with length équal tu the number of rows of the given matrix. 
+#' Each element represents the prediction of fraud for the corresponding row with the tau_EMV_K method.
+#' @export
+#'
+#' @examples
+#' data <- constuct_data2(n = 2000, nsimu = 4)$data
+#' 
+#' multi_EMV_(data)
+multi_EMV_K <- function( mat_X){
+  
+  #fun doit prendre pour valeur:
+  #tau_EMV _ou_ tau_EMV_K_unif _ou_ tau_EMV_K_norm
+  
+  nb_test = DIM(mat_X)[1]
+  y_pred = rep(NA,nb_test)
+  
+  for (j in 1:nb_test) {
+    result = tau_EMV_K(data = mat_X[j,], tresh = 0.2, K = 100)
+    
+    y_pred[j] = result$Fraude
+  }
+  return(list("y_pred"= y_pred))
+}  
+#' tau_EMV_K
+#'
+#'
+#' @description Detects a fraud in the given data serie using the MLE estimator for the time of fraud.
+#'
+#' @param data with rows being vectors of Bernoilli data points
+#' @param tresh the treshold=abs(\hat{p_0}-\hat{p_1}) that is used to make the decision of whether the sample is a fraud or not.
+#' @param K the number of treshold values to skip in order to test 2000/K points instead of 2000. 
+#' 
+#' @return A tibble summarizing the results of the algorithm 
+#' 
+#' @export
+#'
+#' @examples
+#' data <- constuct_data2(n = 2000, nsimu = 1)$data
+#' 
+#' tau_EMV_K(data)
+#' 
 tau_EMV_K <- function(data, tresh = 0.2, K = 100){
   
   fraude = FALSE
@@ -77,6 +137,25 @@ tau_EMV_K <- function(data, tresh = 0.2, K = 100){
   return(res)
 }
 
+#' tau_EMV_K_unif
+#'
+#'
+#' @description Detects a fraud in the given data serie using the MLE estimator for the time of fraud.
+#' The considered values of frud events are drawn from a uniform distribution 
+#'
+#' @param data with rows being vectors of Bernoilli data points
+#' @param tresh the treshold=abs(\hat{p_0}-\hat{p_1}) that is used to make the decision of whether the sample is a fraud or not.
+#' @param K the number of treshold values to skip in order to test 2000/K points instead of 2000. 
+#' 
+#' @return A tibble summarizing the results of the algorithm 
+#' 
+#' @export
+#'
+#' @examples
+#' data <- constuct_data2(n = 2000, nsimu = 1)$data
+#' 
+#' tau_EMV_K_unif(data)
+#' 
 tau_EMV_K_unif <- function(data, tresh = 0.2, K = 100){
   
   fraude = FALSE
@@ -138,7 +217,25 @@ tau_EMV_K_unif <- function(data, tresh = 0.2, K = 100){
   return(res)
 }
 
-
+#' tau_EMV_K_norm
+#'
+#'
+#' @description Detects a fraud in the given data serie using the MLE estimator for the time of fraud.
+#' The considered values of frud events are drawn from a normal distribution with mean=1000 and standard deviation=500
+#'
+#' @param data with rows being vectors of Bernoilli data points
+#' @param tresh the treshold=abs(\hat{p_0}-\hat{p_1}) that is used to make the decision of whether the sample is a fraud or not.
+#' @param K the number of treshold values to skip in order to test 2000/K points instead of 2000. 
+#' 
+#' @return A tibble summarizing the results of the algorithm 
+#' 
+#' @export
+#'
+#' @examples
+#' data <- constuct_data2(n = 2000, nsimu = 1)$data
+#' 
+#' tau_EMV_K_norm(data)
+#' 
 tau_EMV_K_norm <- function(data, tresh = 0.2, K = 100){
   
   fraude = FALSE
@@ -200,7 +297,25 @@ tau_EMV_K_norm <- function(data, tresh = 0.2, K = 100){
   return(res)
 }
 
-#Choix du tau de rupture par maximum de vraissemblance
+#' tau_EMV
+#'
+#'
+#' @description Detects a fraud in the given data serie using the MLE estimator for the time of fraud.
+#' 
+#'
+#' @param data with rows being vectors of Bernoilli data points
+#' @param tresh the treshold=abs(\hat{p_0}-\hat{p_1}) that is used to make the decision of whether the sample is a fraud or not.
+#'  
+#' 
+#' @return A tibble summarizing the results of the algorithm 
+#' 
+#' @export
+#'
+#' @examples
+#' data <- constuct_data2(n = 2000, nsimu = 1)$data
+#' 
+#' tau_EMV(data)
+#' 
 tau_EMV <- function(data, tresh = 0.2, K=100){
   
   fraude = FALSE
