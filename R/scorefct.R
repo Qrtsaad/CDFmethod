d173c4764058c6c34ffe76bfab0d030449a0d9cb
Pablo_lgf <78906882+Pabzer@users.noreply.github.com>
2022-01-27 19:27:26 +0100

    Description fonctions

--- R/scorefct.R
+++ R/scorefct.R
@@ -1,3 +1,16 @@
+#' prec_score
+#'
+#'
+#' @description computes the precision score of the given confusion matrix
+#'
+#' @param mat the confusion matrix
+#'
+#' @return the precision score of the confusion matrix
+#'
+#' @examples
+#' data <- matrix(c(15,2 , 5, 6))
+#' 
+#' prec_score(data)
 prec_score <- function(mat)
 {
   tp <- mat[2,2]
@@ -6,6 +19,19 @@ prec_score <- function(mat)
   return(tp/(tp+fp))
 }
 
+#' rec_score
+#'
+#'
+#' @description computes the recall score of the given confusion matrix
+#'
+#' @param mat the confusion matrix
+#'
+#' @return the recall score of the confusion matrix
+#'
+#' @examples
+#' data <- matrix(c(15,2 , 5, 6))
+#' 
+#' rec_score(data)
 
 rec_score <- function(mat)
 {
@@ -15,6 +41,19 @@ rec_score <- function(mat)
   return(tp/(tp+tn))
 }
 
+#' tpr_score
+#'
+#'
+#' @description computes the true positive rate score of the given confusion matrix
+#'
+#' @param mat the confusion matrix
+#'
+#' @return the true positive rate score of the confusion matrix
+#'
+#' @examples
+#' data <- matrix(c(15,2 , 5, 6))
+#' 
+#' tpr_score(data)
 tpr_score <- function(mat){
   tp <- mat[2,2]
   fn <- mat[1,2] 
@@ -22,6 +61,19 @@ tpr_score <- function(mat){
   return(tp/(tp+fn))
 }
 
+#' sensi_score
+#'
+#'
+#' @description computes the sensibility score of the given confusion matrix
+#'
+#' @param mat the confusion matrix
+#'
+#' @return the true positive rate score of the confusion matrix
+#'
+#' @examples
+#' data <- matrix(c(15,2 , 5, 6))
+#' 
+#' sensi_score(data)
 sensi_score <- function(mat)
 {
   tp <- mat[2,2]
@@ -30,7 +82,19 @@ sensi_score <- function(mat)
   return(tp/(tp+fn))
 }
 
-
+#' specif_score
+#'
+#'
+#' @description computes the specitficity score of the given confusion matrix
+#'
+#' @param mat the confusion matrix
+#'
+#' @return the specitficity score of the confusion matrix
+#'
+#' @examples
+#' data <- matrix(c(15,2 , 5, 6))
+#' 
+#' specif_score(data)
 specif_score <- function(mat)
 {
   tn <- mat[1,1]
@@ -40,7 +104,19 @@ specif_score <- function(mat)
   return(tn/(tn+fp))
 }
 
-
+#' f1_score
+#'
+#'
+#' @description computes the f1 score of the given confusion matrix
+#'
+#' @param mat the confusion matrix
+#'
+#' @return the f1 score of the confusion matrix
+#'
+#' @examples
+#' data <- matrix(c(15,2 , 5, 6))
+#' 
+#' f1_score(data)
 f1_score <- function(mat)
 {
   precision <- prec_score(mat)
@@ -50,7 +126,19 @@ f1_score <- function(mat)
   return(2*(precision*recall)/(precision+recall))
 }
 
-
+#' acc_score
+#'
+#'
+#' @description computes the accuracy score of the given confusion matrix
+#'
+#' @param mat the confusion matrix
+#'
+#' @return the accuracy score of the confusion matrix
+#'
+#' @examples
+#' data <- matrix(c(15,2 , 5, 6))
+#' 
+#' acc_score(data)
 acc_score <- function(mat)
 {
   tn <- mat[1,1]
@@ -62,7 +150,19 @@ acc_score <- function(mat)
   return((tn+tp)/(tn+fn+fp+tp))
 }
 
-
+#' tss_score
+#'
+#'
+#' @description computes the True skill statistic of the given confusion matrix
+#'
+#' @param mat the confusion matrix
+#'
+#' @return the True skill statistic of the confusion matrix
+#'
+#' @examples
+#' data <- matrix(c(15,2 , 5, 6))
+#' 
+#' tss_score(data)
 tss_score <- function(mat)
 {
   sensi <- sensi_score(mat)
@@ -93,7 +193,25 @@ nid_score <- function(y, y_pred)
   return(scores$NID)
 }
 
-
+#' compute_mf
+#'
+#'
+#' @description computes the a confusio matrix from two vectors: one of labels and one of predictions, and computes a score
+#' 
+#' @param v_ver the labels
+#' @param v_est the predictions
+#' @param score the score to compute. Must be in: c("f1", "accuracy", "tp_rate","specificite", "sensibilite", "tss", "precision", "recall")
+#' 
+#' 
+#' @return a list with the confusion matrix and the computed score
+#'
+#' @examples
+#' data <- constuct_data2(n = 200, nsimu = 2)
+#' labels <- data$data[1]
+#' predictions <- data$data[2]
+#' 
+#' compute_mf(data)$Confusion
+#' compute_mf(data)$score
 compute_mf <- function(v_ver,v_est, score = "f1")
 {
   
